bceb579b0a74eba81436b55db40df9c81154b674
IAmHim89 <computertech0418@gmail.com>
2024-06-22 16:30:06 -0400

    Assignment completed

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const computerResponse = document.querySelector("#computer-response");
+const nameForm = document.querySelector("#name-form");
+const userInput = document.querySelector("#user-input");
+
+nameForm.addEventListener("submit", (e) => {
+  e.preventDefault(); //prevents for from refreshing use without action value
+  const value = userInput.value;
+  const enemiesArray = ["Joker", "Juggernaut", "Venom", "Dabura", "Frieza"];
+
+  if (enemiesArray.includes(value)) {
+    computerResponse.textContent = "Go Away!";
+  } else {
+    computerResponse.textContent = "Hello" + " " + value + "!";
+  }
+});
